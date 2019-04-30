951274e95808b140a7569d03bb45a6c17b8ad675
udaych31 <hariharamohanty@gmail.com>
2019-04-30 17:16:31 +0530

    login user api

--- src/main/java/com/hcl/ing/pet/app/controller/UserController.java
+++ src/main/java/com/hcl/ing/pet/app/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.hcl.ing.pet.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hcl.ing.pet.app.dto.UserDto;
+import com.hcl.ing.pet.app.dto.UserResponse;
 import com.hcl.ing.pet.app.service.UserService;
 
 @RestController
@@ -25,5 +31,16 @@ public class UserController {
 			return "User not created";
 
 	}
-
+	
+	
+	
+	@GetMapping("/user/login")
+	public ResponseEntity<String> loginUser(@RequestParam String mailId,@RequestParam String password){
+		
+		 UserResponse response=userService.loginUser(mailId, password);
+		return new ResponseEntity<String>(response.getResponse(),HttpStatus.CREATED);
+	}
+	
+	
+	
 }
